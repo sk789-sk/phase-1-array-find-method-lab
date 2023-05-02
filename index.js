42e69f7ed7371aa9797ee9129cc7d9f6969ab0a2
Shams <skm478@gmail.com>
2023-05-02 17:43:21 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+
 // code your solution here
+/* just uses a loop doesnt use find the way the lab wanted */
+
+function superbowlWins(Array){ 
+    for (let i=0 ; i < Array.length; i++) {
+        if (Array[i].result === "W"){
+             return Array[i].year
+        } 
+    }
+    return undefined
+}
+
+
+function isWin(element){
+    return element.result === 'W'
+}
+
+function superbowlWin(array){
+    let out = array.find(isWin)  // Is there a way for me to output something other than the first element 
+    return out ? out.year : undefined;
+    //if (out === undefined){
+    //    return out}
+    //return out.year
+}
+
+
+const record = [
+    { year: "2015", result: "L"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "W"}, ]
+
+console.log(superbowlWin(record))
+
